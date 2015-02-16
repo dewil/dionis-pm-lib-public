3b7ebd469c735a3bdf72176382a85167bc7408b9
dwl <dewil@dewil.ru>
2015-02-17 01:46:20 +0300

    update description

--- pm.php
+++ pm.php
@@ -1,26 +1,23 @@
 <?php
 
 /*
- * http proxy via curl
+ * pm library
  * ver 1.0 2015-02-16
  * @author dwl dewil@dewil.ru
- * @package http proxy
+ * @package
 */
 
 if (!function_exists('curl_setopt_array')) {
-    echo 'no curl';
-    exit;
-}
-if ($param = @json_decode(file_get_contents('php://input'), true)) {
-    $ch = curl_init();
-    curl_setopt_array($ch, $param);
-    $result = curl_exec($ch);
-    $out = array();
-    $out['param'] = $param;
-    $out['getinfo'] = curl_getinfo($ch);
-    $out['result'] = $result;
-
-    echo json_encode($out, true);
+	echo 'no curl';
+} elseif ($param = @json_decode(file_get_contents('php://input'), true)) {
+	$ch = curl_init();
+	curl_setopt_array($ch, $param);
+	$result = curl_exec($ch);
+	$out = array();
+	$out['param'] = $param;
+	$out['getinfo'] = curl_getinfo($ch);
+	$out['result'] = $result;
+	echo json_encode($out, true);
 } else {
-    echo 'bad request';
+	echo 'bad request';
 }
