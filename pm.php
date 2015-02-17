8b11a7bd3be2bce601cef5f5e1085c8b88275978
dwl <dewil@dewil.ru>
2015-02-18 00:10:20 +0300

    update check json module

--- pm.php
+++ pm.php
@@ -2,13 +2,15 @@
 
 /*
  * pm library
- * ver 1.0 2015-02-16
+ * ver 1.01 2015-02-18
  * @author dwl dewil@dewil.ru
  * @package
 */
 
 if (!function_exists('curl_setopt_array')) {
-	echo 'no curl';
+	echo 'oops! no php-curl module';
+} elseif (!function_exists('json_encode')) {
+	echo 'oops! no php-json module';
 } elseif ($param = @json_decode(file_get_contents('php://input'), true)) {
 	$ch = curl_init();
 	curl_setopt_array($ch, $param);
@@ -19,6 +21,6 @@ if (!function_exists('curl_setopt_array')) {
 	$out['result'] = $result;
 	echo json_encode($out, true);
 } else {
-	echo 'install ok. waiting command.';
+	echo 'install ok. waiting command...';
 }
 ?>
